c689ef59c865a7b8b1302e2ba1fd543343357301
Omar Redani <omar.redani1@gmail.com>
2021-11-15 22:19:07 +0100

    PagingSource-&-PagingData

--- app/src/main/java/com/ibm/movieappmvvm/ui/gallery/MovieViewModel.kt
+++ app/src/main/java/com/ibm/movieappmvvm/ui/gallery/MovieViewModel.kt
@@ -1,10 +1,29 @@
 package com.ibm.movieappmvvm.ui.gallery
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.ibm.movieappmvvm.data.MovieRepository
 
 class MovieViewModel @ViewModelInject constructor(
     private val repository: MovieRepository
 ) : ViewModel() {
+
+    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+
+    val movies = currentQuery.switchMap { queryString ->
+        repository.getSearchResults(queryString).cachedIn(viewModelScope)
+    }
+
+    fun searchMovies(query: String) {
+        currentQuery.value = query
+    }
+
+    companion object {
+        private const val DEFAULT_QUERY = "release_date.desc"
+    }
+
 }
