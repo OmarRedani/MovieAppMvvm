1b2f16b8d69121a3155e2e9558183cf7dea1fcc3
Omar Redani <omar.redani1@gmail.com>
2021-11-15 22:28:04 +0100

    PagingDataAdapter

---
+++ app/src/main/java/com/ibm/movieappmvvm/ui/gallery/MovieAdapter.kt
@@ -0,0 +1,59 @@
+package com.ibm.movieappmvvm.ui.gallery
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.ibm.movieappmvvm.R
+import com.ibm.movieappmvvm.api.MovieApi
+import com.ibm.movieappmvvm.data.MovieModel
+import com.ibm.movieappmvvm.databinding.ItemMovieBinding
+
+class MovieAdapter :
+    PagingDataAdapter<MovieModel, MovieAdapter.MovieViewHolder>(PHOTO_COMPARATOR) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val binding =
+            ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return MovieViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val currentItem = getItem(position)
+
+        if (currentItem != null) {
+            holder.bind(currentItem)
+        }
+    }
+
+    class MovieViewHolder(private val binding: ItemMovieBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(movie: MovieModel) {
+            binding.apply {
+                Glide.with(itemView)
+                    .load("${MovieApi.BASE_POSTER_URL}${movie.poster_path}")
+                    .centerCrop()
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .error(R.drawable.ic_error)
+                    .into(imageView)
+
+                textViewTitle.text = movie.title
+            }
+        }
+    }
+
+    companion object {
+        private val PHOTO_COMPARATOR = object : DiffUtil.ItemCallback<MovieModel>() {
+            override fun areItemsTheSame(oldItem: MovieModel, newItem: MovieModel) =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: MovieModel, newItem: MovieModel) =
+                oldItem == newItem
+        }
+    }
+}
--- app/src/main/java/com/ibm/movieappmvvm/ui/gallery/MoviesFragment.kt
+++ app/src/main/java/com/ibm/movieappmvvm/ui/gallery/MoviesFragment.kt
@@ -5,19 +5,37 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.ibm.movieappmvvm.R
+import com.ibm.movieappmvvm.databinding.FragmentMoviesBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MoviesFragment:Fragment(R.layout.fragment_movies) {
+class MoviesFragment : Fragment(R.layout.fragment_movies) {
 
     private val viewModel by viewModels<MovieViewModel>()
+
+    private var _binding: FragmentMoviesBinding? = null
+    private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.movies.observe(viewLifecycleOwner){
+        _binding = FragmentMoviesBinding.bind(view)
 
+        val adapter = MovieAdapter()
+
+        binding.apply {
+            recyclerView.setHasFixedSize(true)
+            recyclerView.adapter = adapter
         }
+
+        viewModel.movies.observe(viewLifecycleOwner) {
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
